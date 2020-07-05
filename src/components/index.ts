796d62d74b770faf69b2a8558d8e46dc485db786
ffarrzzz <9x2p23d923@protonmail.com>
2020-07-05 14:46:08 +0000

    Update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { EditorNavbar } from "./EditorNavbar";
 export { EditorTabs } from "./EditorTabs";
 export { EditorCanvas } from "./EditorCanvas";
+
+export { MainNavbar } from "./MainNavbar";
