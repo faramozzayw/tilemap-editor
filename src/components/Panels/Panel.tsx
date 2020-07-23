994ef4888100b95a965a4f04f7b0e2c84956e8a5
faramo_zayw <faramo.zayw@gmail.com>
2020-07-23 14:48:14 +0000

    update panel logic

--- src/components/Panels/Panel.tsx
+++ src/components/Panels/Panel.tsx
@@ -4,22 +4,24 @@ import classnames from "classnames";
 
 import "./index.css";
 
-import {
-	Panel as BulmaPanel,
-	PanelHeading,
-	PanelBlock,
-	PanelIcon,
-	Button,
-} from "./../../bulma";
-import { editorStore, setCurrentTerrain } from "./../../store/editorStore";
-
-import { BaseTerrainEnum } from "./../../types";
+import { Panel as BulmaPanel, PanelHeading, Button } from "./../../bulma";
+import { editorStore } from "./../../store/editorStore";
+import { TerrainPanel } from "./TerrainPanel";
 
 export const Panel = () => {
 	const { currentInstrument, currentBaseTerrain } = useStore(editorStore);
 	const [isOpenPanel, setOpenPanel] = useState(true);
 
 	const togglePanel = () => setOpenPanel(!isOpenPanel);
+
+	const getCurrentPanel = () => {
+		switch (currentInstrument as string) {
+			case "Terrain":
+				return <TerrainPanel isOpenPanel={isOpenPanel} />;
+			default:
+				return null;
+		}
+	};
 
 	return (
 		<BulmaPanel isColor="info" className="canvas-panel">
@@ -38,77 +40,7 @@ export const Panel = () => {
 					<strong>{isOpenPanel ? "Close" : "Open"} panel</strong>
 				</Button>
 			</PanelHeading>
-			<div className="panel-body">
-				{isOpenPanel && (
-					<>
-						<PanelBlock className="panel-select">
-							<p className="control has-icons-left">
-								<input className="input" type="text" placeholder="Search" />
-								<span className="icon is-left">
-									<i className="fas fa-search" aria-hidden="true"></i>
-								</span>
-							</p>
-						</PanelBlock>
-						<PanelBlock isActive className="panel-select">
-							<PanelIcon icon="fas fa-book" />
-							Base terrain
-							<div className="select">
-								<select
-									value={currentBaseTerrain}
-									onChange={(e) => {
-										// @ts-ignore
-										setCurrentTerrain(e.target.value);
-									}}
-								>
-									{Object.keys(BaseTerrainEnum).map((terrain) => (
-										<option value={terrain} key={terrain}>
-											{terrain}
-										</option>
-									))}
-								</select>
-							</div>
-						</PanelBlock>
-						<PanelBlock className="panel-select">
-							<PanelIcon icon="fas fa-book" />
-							Feature terrain
-							<div className="select">
-								<select>
-									{Object.keys(BaseTerrainEnum).map((terrain) => (
-										<option value={terrain} key={terrain}>
-											{terrain}
-										</option>
-									))}
-								</select>
-							</div>
-						</PanelBlock>
-						<PanelBlock>
-							<PanelIcon icon="fas fa-book" />
-							minireset.css
-						</PanelBlock>
-						<PanelBlock>
-							<PanelIcon icon="fas fa-book" />
-							jgthms.github.io
-						</PanelBlock>
-						<PanelBlock>
-							<PanelIcon icon="fas fa-code-branch" />
-							daniellowtw/infboard
-						</PanelBlock>
-						<PanelBlock>
-							<PanelIcon icon="fas fa-code-branch" />
-							mojs
-						</PanelBlock>
-						<PanelBlock>
-							<input type="checkbox" />
-							remember me
-						</PanelBlock>
-						<PanelBlock className="is-radiusless">
-							<button className="button is-link is-outlined is-fullwidth">
-								Reset all
-							</button>
-						</PanelBlock>
-					</>
-				)}
-			</div>
+			{getCurrentPanel()}
 		</BulmaPanel>
 	);
 };
