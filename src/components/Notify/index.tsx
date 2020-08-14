d5e3b16bd90548e50ee5b631d543303522cdfd51
faramozzayw <faramo.zayw@gmail.com>
2020-08-14 17:41:39 +0000

    Update  notify style

--- src/components/Notify/index.tsx
+++ src/components/Notify/index.tsx
@@ -7,10 +7,47 @@ import NotifyStyle from "./Notify.module.css";
 export const NotifyLayout: React.FC = () => {
 	return (
 		<Portal className={NotifyStyle.wrapper}>
-			<div className={classnames(NotifyStyle.zIndex)}>
+			<div className={classnames(NotifyStyle.notifyList)}>
 				<div className="notification is-danger is-light">
 					<button className="delete"></button>
-					Primar lorem ipsum dolor sit amet, consectetur
+					Primar lorem ipsum dolor sit amet, consectetur 1
+				</div>
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 2
+				</div>
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 1
+				</div>
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 2
+				</div>
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 1
+				</div>
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 2
+				</div>
+
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 1
+				</div>
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 2
+				</div>
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 1
+				</div>
+				<div className="notification is-danger is-light">
+					<button className="delete"></button>
+					Primar lorem ipsum dolor sit amet, consectetur 2
 				</div>
 			</div>
 		</Portal>
