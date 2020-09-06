f929cc421f6d0efc839525c759aa4f57b532ebda
faramozzayw <faramo.zayw@gmail.com>
2020-09-06 13:29:16 +0000

    Update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,3 @@
-export { EditorNavbar } from "./Editor/EditorNavbar";
-export { EditorTabs } from "./Editor/EditorTabs";
-export { EditorCanvas } from "./Editor/EditorCanvas";
-
 export { MainNavbar } from "./MainNavbar";
 export { MapPreviewCard } from "./MapPreviewCard";
 export { NotifyLayout } from "./Notify";
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
-export { Editor } from "./Editor";
 export { Main } from "./Main";
 export { Map } from "./Map";
 export { ProfilePage } from "./Profile";
