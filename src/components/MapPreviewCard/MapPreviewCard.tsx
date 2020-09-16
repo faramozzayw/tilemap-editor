bac4aa6d973a26d3720a0807b10df79de7bfe357
faramo_zayw <40602472+faramozzayw@users.noreply.github.com>
2020-09-16 19:49:29 +0300

    Preview map update (#33)
    * Update preview map
    
    * updated tests up to date

--- src/components/MapPreviewCard/MapPreviewCard.tsx
+++ src/components/MapPreviewCard/MapPreviewCard.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { CSSProperties, useState, useEffect } from "react";
 import { useHistory, Link } from "react-router-dom";
+import { Transition } from "react-transition-group";
 
 import { Title, Card, CardContent } from "./../../bulma";
 
@@ -12,6 +13,20 @@ import { Map } from "../../types/graphql";
 
 export interface MapPreviewCardProps extends Map, IAuth {}
 
+const duration = 200;
+
+const defaultStyle: CSSProperties = {
+	transition: `opacity ${duration}ms ease-in-out`,
+	opacity: 0,
+};
+
+const transitionStyles: any = {
+	entering: { opacity: 1 },
+	entered: { opacity: 1 },
+	exiting: { opacity: 0 },
+	exited: { opacity: 0 },
+};
+
 export const MapPreviewCard: React.FC<MapPreviewCardProps> = ({
 	author,
 	name,
@@ -20,6 +35,12 @@ export const MapPreviewCard: React.FC<MapPreviewCardProps> = ({
 	...props
 }) => {
 	const history = useHistory();
+	const [inProp, setInProp] = useState(false);
+	const [liked, setLiked] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => setInProp(true), duration);
+	}, [setInProp]);
 
 	if (!author) {
 		return null;
@@ -30,39 +51,62 @@ export const MapPreviewCard: React.FC<MapPreviewCardProps> = ({
 	const viewHandler = () => history.push(`/maps/${id}`);
 
 	return (
-		<Card className="MapPreviewCard has-background-grey-dark has-text-primary-light is-clipped">
-			<CardContent>
-				<div className="media">
-					<div className="media-left">
-						<figure className="image is-48x48">
-							<img
-								src="https://bulma.io/images/placeholders/96x96.png"
-								alt="Test image"
-							/>
-						</figure>
-					</div>
-					<div className="media-content">
-						<Title isSize={4}>
-							<Link to={`/maps/${id}`}>
-								<MapName name={name} />
-							</Link>
-						</Title>
-						<Title tag="p" isSubtitle isSize={6}>
-							<UserLink username={author.username} />
-						</Title>
-					</div>
-				</div>
+		<Transition in={inProp} timeout={duration}>
+			{(state) => (
+				<Card
+					className="MapPreviewCard has-background-grey-dark has-text-primary-light is-clipped"
+					tag="article"
+					style={{
+						...defaultStyle,
+						...transitionStyles[state],
+					}}
+				>
+					<CardContent>
+						<div className="media">
+							<div className="media-left">
+								<figure className="image is-48x48">
+									<img
+										src="https://bulma.io/images/placeholders/96x96.png"
+										alt="Test image"
+									/>
+								</figure>
+							</div>
+							<div className="media-content">
+								<Title isSize={4}>
+									<Link to={`/maps/${id}`}>
+										<MapName name={name} />
+									</Link>
+								</Title>
+								<Title tag="p" isSubtitle isSize={6}>
+									<UserLink username={author.username} />
+								</Title>
+							</div>
+							<div className="media-right">
+								<button
+									className="like-button"
+									onClick={() => setLiked((prev) => !prev)}
+								>
+									{liked ? (
+										<i className="fas fa-heart has-text-danger"></i>
+									) : (
+										<i className="fas fa-heart-broken has-text-grey-light"></i>
+									)}
+								</button>
+							</div>
+						</div>
 
-				<PreviewCardInfo {...props} />
-			</CardContent>
-			{isAuth && (
-				<PreviewCardFooter
-					id={id}
-					viewHandler={viewHandler}
-					editHandler={editHandler}
-					forkHandler={forkHandler}
-				/>
+						<PreviewCardInfo {...props} />
+					</CardContent>
+					<PreviewCardFooter
+						id={id}
+						author={author}
+						isAuth={isAuth}
+						viewHandler={viewHandler}
+						editHandler={editHandler}
+						forkHandler={forkHandler}
+					/>
+				</Card>
 			)}
-		</Card>
+		</Transition>
 	);
 };
--- src/test/PreviewCardFooter.spec.js
+++ src/test/PreviewCardFooter.spec.js
@@ -3,19 +3,25 @@ import { render } from "@testing-library/react";
 
 import { PreviewCardFooter } from "./../components/MapPreviewCard";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "../hooks/auth";
 
 const props = {
 	id: "iu2h3u284u2s9-234242s2342",
 	editHandler: () => {},
 	forkHandler: () => {},
 	viewHandler: () => {},
+	author: {
+		id: "e8b1c3aa-e088-4129-8ca0-358fee0cb480",
+	},
 };
 
 test("renders correctly", () => {
 	const { baseElement } = render(
-		<BrowserRouter>
-			<PreviewCardFooter {...props} />
-		</BrowserRouter>,
+		<AuthProvider>
+			<BrowserRouter>
+				<PreviewCardFooter {...props} />
+			</BrowserRouter>
+		</AuthProvider>,
 	);
 
 	expect(baseElement).toMatchSnapshot();
