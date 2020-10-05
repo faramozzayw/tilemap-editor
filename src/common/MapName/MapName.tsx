83e3dc09c1413725a6969e3cfd0946ae8afb20f2
faramozzayw <faramo.zayw@gmail.com>
2020-10-05 17:27:09 +0000

    Update

--- src/common/MapName/MapName.tsx
+++ src/common/MapName/MapName.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 import Styles from "./MapName.module.css";
+import { Link, LinkProps } from "react-router-dom";
 
-export interface MapName {
+export interface MapName extends Partial<LinkProps> {
 	name: string;
 }
 
-export const MapName: React.FC<MapName> = ({ name }) => (
-	<span className={Styles.MapName}>{name}</span>
-);
+export const MapName: React.FC<MapName> = ({ name, to, ...props }) =>
+	to ? (
+		<Link to={to} className={Styles.MapName} {...props}>
+			{name}
+		</Link>
+	) : (
+		<span className={Styles.MapName} {...props}>
+			{name}
+		</span>
+	);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import "bulma/css/bulma.css";
+import "bulma/css/bulma.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 import App from "./App";
