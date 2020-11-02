69fca207a4ec26c2cdf21e47ff6873545257eea9
faramozzayw <faramo.zayw@gmail.com>
2020-11-02 15:12:47 +0000

    up

--- src/components/AvatarMenu/index.tsx
+++ src/components/AvatarMenu/index.tsx
@@ -4,6 +4,7 @@ import {
 	NavbarDropdown,
 	NavbarDivider,
 	Image,
+	Button,
 } from "@faramo.zayw/reabulma";
 
 import "./AvatarMenu.css";
@@ -11,6 +12,7 @@ import "./AvatarMenu.css";
 import { LogOut } from "./../AuthForms";
 import { UserLink } from "../../common";
 import { User } from "../../types/graphql";
+import { Link } from "react-router-dom";
 
 export interface AvatarMenuProps extends Pick<User, "imageUrl" | "username"> {}
 
@@ -30,7 +32,9 @@ const AvatarMenu: React.FC<AvatarMenuProps> = ({ username, imageUrl }) => (
 			<NavbarItem>
 				<UserLink username={username} />
 			</NavbarItem>
-			<NavbarDivider />
+			<NavbarItem>
+				<Link to="/me/setting">Setting</Link>
+			</NavbarItem>
 			<NavbarItem>
 				<LogOut />
 			</NavbarItem>
